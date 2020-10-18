0ff28671bffb957cabd0037b4e9a95b0406f27d2
madarshahian86@gmail.com <mahyar-madarshahian>
2020-10-18 19:21:30 +0330

    this is my second heuristic algorithm

---
+++ Kashan-method2/method2_leaf_paths.py
@@ -0,0 +1,27 @@
+import prim_algorithm
+import pandas as pd
+import operator
+from functools import reduce   
+leafage=[]
+primary_leaf_paths=[]
+prim_leaf_paths=[]
+for i in range(sub_G.number_of_nodes()):
+    if sub_G.degree[i]==1:
+        leafage.append(i)
+print(leafage)
+for i in range(len(leafage)):
+    for j in range(len(leafage)):
+        paths = list(nx.shortest_simple_paths(sub_G, leafage[i], leafage[j]))
+        primary_leaf_paths.append(paths)
+# چون خروجی کد بالا سه لیست تو در تو است با استفاده از تابع زیر لیست بیرونی حذف می شود.
+primary_leaf_paths=reduce(operator.concat,primary_leaf_paths)  
+for item in primary_leaf_paths:
+    if len(item)>1:
+        prim_leaf_paths.append(item)  
+output_file=open("prim_leaf_paths","w")
+for item in prim_leaf_paths:
+    for item2 in item:
+        
+        # به دلیل اینکه در الگوریتم ژنتیک نقاط از 1 تا 15 است ولی در اینجا 0 تا 14 است.
+        output_file.writelines(f'{item2+1}\t')
+    output_file.writelines(f'\n[TYPE 3]\t')
